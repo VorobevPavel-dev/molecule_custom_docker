ac118f341e73f2684de5e31d921eb15eba022088
VorobevPavel-dev <work.pas.vorobiov@gmail.com>
2023-07-26 12:47:14 +0300

    Login command now uses ID instead of container name

--- src/molecule_custom_docker/driver.py
+++ src/molecule_custom_docker/driver.py
@@ -1,4 +1,4 @@
-from molecule import logger
+from molecule import logger, util
 from molecule.api import Driver
 
 LOG = logger.get_logger(__name__)
@@ -24,13 +24,20 @@ class CustomDocker(Driver):
     @property
     def required_collections(self):
         return {"community.docker": "3.4.6"}
+    
+    def _get_instance_config(self, instance_name):
+        instance_config_dict = util.safe_load_file(self._config.driver.instance_config)
+        return next(
+            item for item in instance_config_dict if item["instance"] == instance_name
+        )
 
     def ansible_connection_options(self, instance_name):
         x = {"ansible_connection": "community.docker.docker"}
         return x
 
     def login_options(self, instance_name):
-        return {"instance": instance_name}
+        instance_config = self._get_instance_config(instance_name)
+        return {"id": instance_config["ID"]}
 
     @property
     def login_cmd_template(self):
@@ -40,5 +47,5 @@ class CustomDocker(Driver):
             "-e LINES={lines} "
             "-e TERM=bash "
             "-e TERM=xterm "
-            "-ti {instance} bash"
+            "-ti {id} bash"
         )
